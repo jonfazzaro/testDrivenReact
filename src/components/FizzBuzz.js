de113ac32755f2dd265553d89b241861b48fe2be
Jon Fazzaro <jon@fazzaro.com>
2021-10-12 16:50:23 -0400

    feat: make the FB component more keyboard-friendly

--- src/components/FizzBuzz.js
+++ src/components/FizzBuzz.js
@@ -1,15 +1,31 @@
-import {React, useState} from 'react';
-import Player from '../domain/fizzBuzzPlayer';
+import { React, useState } from 'react'
+import Player from '../domain/fizzBuzzPlayer'
 
 function FizzBuzz() {
-    const [input, setInput] = useState(null);
-    const [result, setResult] = useState("");
+  const [input, setInput] = useState(null)
+  const [result, setResult] = useState('')
 
-    return <div data-testid="fb-container">
-        <input type="number" data-testid="fb-input" onChange={e => setInput(e.target.value)}/>
-        <input type="submit" data-testid="fb-command" onClick={e => setResult(Player.play(input))}/>
-        <div data-testid="fb-result">{result}</div>
-    </div>;
+  return (
+    <div data-testid="fb-container">
+      <form onSubmit={submit}>
+        <input
+          type="number"
+          data-testid="fb-input"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <input
+          type="submit"
+          data-testid="fb-command"
+        />
+      </form>
+      <div data-testid="fb-result">{result}</div>
+    </div>
+  )
+
+  function submit(event) {
+    event.preventDefault();
+    setResult(Player.play(input));
+  }
 }
 
-export default FizzBuzz;
+export default FizzBuzz
