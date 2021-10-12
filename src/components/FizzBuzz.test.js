143295c774032d0bfeb385203cfe0000e5b54166
Jon Fazzaro <jon@fazzaro.com>
2021-10-12 16:37:18 -0400

    clean: refactor React tests

--- src/components/FizzBuzz.test.js
+++ src/components/FizzBuzz.test.js
@@ -1,48 +1,56 @@
-jest.mock("../fizzBuzzPlayer");
+jest.mock('../fizzBuzzPlayer')
 
-import { render, fireEvent } from '@testing-library/react';
-import Player from '../fizzBuzzPlayer';
-import FizzBuzz from './FizzBuzz';
+import { render, fireEvent } from '@testing-library/react'
+import Player from '../fizzBuzzPlayer'
+import FizzBuzz from './FizzBuzz'
 
-Player.play = jest.fn();
-
-describe("The FizzBuzz component", () => {
-  let _subject;
+describe('The FizzBuzz component', () => {
+  let _subject
 
   beforeEach(() => {
-    Player.play.mockReturnValue("RESULT");
-    _subject = render(<FizzBuzz />);
-  });
+    Player.play = jest.fn()
+    Player.play.mockReturnValue('RESULT')
+    _subject = render(<FizzBuzz />)
+  })
 
-  it("plays fizz buzz with the Player", () => {
-    const container = _subject.getByTestId("fb-result");
-    expect(container.textContent).toEqual("");
-  });
+  it('does not show a result yet', () => {
+    showsResult('')
+  })
 
-  it('has a text box', () => {
-    const input = _subject.getByTestId("fb-input");
-    expect(input).toBeInTheDocument();
-    expect(input).toHaveAttribute("type", "number");
-  });
+  it('has a number text box', () => {
+    hasInput('fb-input', 'number')
+  })
 
   it('has a button', () => {
-    const command = _subject.getByTestId("fb-command");
-    expect(command).toBeInTheDocument();
-    expect(command).toHaveAttribute("type", "submit");
-  });
+    hasInput('fb-command', 'submit')
+  })
 
-  it('when played, displays the result', () => {
-    const result = _subject.getByTestId("fb-result");
-    const input = _subject.getByTestId("fb-input");
-    const command = _subject.getByTestId("fb-command");
-    Player.play.mockReturnValue("fizz");
+  describe('when played', () => {
+    it('displays the result', () => {
+      Player.play.mockReturnValue('fizz')
+      enter(3)
+      submit()
+      showsResult('fizz')
+    })
+  })
 
-    fireEvent.change(input, {target: {value: '3'}})
-    fireEvent.click(command);
-    
-    expect(result).toHaveTextContent("fizz");
-  });
+  function hasInput(testid, type) {
+    const input = _subject.getByTestId(testid)
+    expect(input).toHaveAttribute('type', type)
+  }
 
+  function showsResult(expected) {
+    const result = _subject.getByTestId('fb-result')
+    expect(result).toHaveTextContent(expected)
+  }
 
+  function enter(number) {
+    const input = _subject.getByTestId('fb-input')
+    fireEvent.change(input, { target: { value: number } })
+  }
 
-});
+  function submit() {
+    const command = _subject.getByTestId('fb-command')
+    fireEvent.click(command)
+  }
+})
--- src/fizzBuzzPlayer.js
+++ src/fizzBuzzPlayer.js
@@ -1,15 +1,14 @@
-export default { play }
+export default { play };
 
 function play(input) {
   return fizz(input) + buzz(input) || input.toString();
 }
 
-const fizz = word.bind(null, 3, 'fizz'); 
-const buzz = word.bind(null, 5, 'buzz'); 
+const fizz = word.bind(null, 3, 'fizz');
+const buzz = word.bind(null, 5, 'buzz');
 
 function word(factor, noise, input) {
-    if (input % factor === 0)
-        return noise;
+  if (input % factor === 0) return noise
 
-    return "";
+  return ''
 }
