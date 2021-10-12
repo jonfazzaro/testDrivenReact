5e4b5f783d959b488533deff4b43a06101127bce
Jon Fazzaro <jon@fazzaro.com>
2021-10-12 15:22:39 -0400

    clean: FP AF

--- src/fizzBuzzPlayer.js
+++ src/fizzBuzzPlayer.js
@@ -1,12 +1,15 @@
 export default { play }
 
 function play(input) {
-  return fizz(input) + buzz(input) 
-       || input.toString();
+  return fizz(input) + buzz(input) || input.toString();
 }
 
-const fizz = i => word(i, 3, 'fizz');
-const buzz = i => word(i, 5, 'buzz');
+const fizz = word.bind(null, 3, 'fizz'); 
+const buzz = word.bind(null, 5, 'buzz'); 
 
-const word = (input, factor, result) =>
-    (input % factor === 0) ? result : "";
+function word(factor, noise, input) {
+    if (input % factor === 0)
+        return noise;
+
+    return "";
+}
