f212ee3580169e7b1d0af86a859c4927af0ad579
Jon Fazzaro <jon@fazzaro.com>
2021-11-29 15:31:45 -0500

    fix: the error

--- src/domain/fizzBuzzPlayer.js
+++ src/domain/fizzBuzzPlayer.js
@@ -4,7 +4,7 @@ function play(input) {
   return fizz(input) + buzz(input) || input.toString();
 }
 
-const fizz = word.bind(null, 2, 'fizz');
+const fizz = word.bind(null, 3, 'fizz');
 const buzz = word.bind(null, 5, 'buzz');
 
 function word(factor, noise, input) {
